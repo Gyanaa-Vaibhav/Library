0d8a364e3ddf5cb1e3bad07a6ee66625501446e8
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-10-08 16:39:12 +0530

    Project Completed

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const bookList = document.getElementById("bookList")
-let bookStorage = []
+const module = document.getElementById("module")
+const btn = document.getElementById("newBook");
+const addBookBtn = document.getElementById("addBookBtn");
+const clearAll = document.getElementById("clearAll");
+let bookStorage = [];
+const a = JSON.parse(localStorage.getItem("bookStorage"));
+
+
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -8,24 +15,66 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
-
-const hobbit = new Book("Hobbit", "J.R.R Tolkien", 295, "Not yet read");
-const hobbit2 = new Book("Hobbit the 2", "J.R.R ", 300, "Not yet read");
-
-bookStorage.push(hobbit)
-bookStorage.push(hobbit2)
-
-console.log(bookStorage)
-
-bookStorage.forEach((x) => { console.log(x.title) })
+if (a) {
+    addBook()
+}
 
 function addBook() {
+
+    console.log(a)
+    bookStorage = JSON.parse(localStorage.getItem("bookStorage"));
+    bookList.innerHTML = ''
     bookStorage.forEach((x) => {
-        bookList.innerHTML += `<p>${x.title}</p>`
+        bookList.innerHTML += `
+            <div class="card">
+            <h3>Title : <p>&nbsp;${x.title}</p> </h3>
+            <h3>Author :<p>&nbsp;${x.author}</p></h3>
+            <h3>Pages : <p>&nbsp;${x.pages}</p></h3>
+            <h3 id="status">Status : <p>&nbsp;${x.status}</p>
+                <a><svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <title>check-circle-outline</title>
+                    <path
+                        d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M12 20C7.59 20 4 16.41 4 12S7.59 4 12 4 20 7.59 20 12 16.41 20 12 20M16.59 7.58L10 14.17L7.41 11.59L6 13L10 17L18 9L16.59 7.58Z" />
+                </svg>
+            </a>
+            </h3>
+        </div>
+
+        `;
     });
 }
 
-addBook()
 
-let x = document.getElementById("title")
+btn.addEventListener("click", function () {
+    module.style.display = "flex"
+})
 
+module.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const name = new Book(title.value, author.value, pages.value, (read.checked ? "Read" : "Not Yet Read"))
+    bookStorage.push(name)
+    localStorage.setItem("bookStorage", JSON.stringify(bookStorage));
+    // console.log(title.value)
+    // console.log(author.value)
+    // console.log(pages.value)
+    // console.log(read.checked);
+    
+    addBook()
+    module.style.display = "none";
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+    read.checked = ''
+});
+
+clearAll.addEventListener("click", () => { localStorage.clear(); addBook(); a=undefined})
+
+
+/*
+const hobbit = new Book("Hobbit", "J.R.R Tolkien", 295, "Read");
+const hobbit2 = new Book("Hobbit the 2", "J.R.R ", 300, "Not yet read");
+bookStorage.push(hobbit)
+bookStorage.push(hobbit2)
+console.log(bookStorage)
+bookStorage.forEach((x) => { console.log(x.title) })
+*/
